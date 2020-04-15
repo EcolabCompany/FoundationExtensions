9798981740c6ca8e35cd82ef04dfe2409c28f9ec
Nate <nathan.mann@me.com>
2020-04-15 16:19:15 -0400

    removed old functions

--- FoundationExtensions/String Extensions.swift
+++ FoundationExtensions/String Extensions.swift
@@ -12,30 +12,4 @@ extension String {
         return self.trimmingCharacters(in: .newlines)
     }
 
-    
-    public func trimCharactersFromFront(_ characters: [Character]) -> String {
-        var startingIndex: Index = self.startIndex
-        for letter in self {
-            if characters.contains(letter) {
-                startingIndex = self.index(startingIndex, offsetBy: 1)
-            } else {
-                break
-            }
-        }
-        return String(self.suffix(from: startingIndex))
-    }
-
-
-    public func trimCharactersFromEnd(_ characters: [Character]) -> String {
-        var endingIndex: Index = self.endIndex
-        for letter in self.reversed() {
-            if characters.contains(letter) {
-                endingIndex = self.index(endingIndex, offsetBy: -1)
-            } else {
-                break
-            }
-        }
-        return String(self.prefix(upTo: endingIndex))
-    }
-    
 }
