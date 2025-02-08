2af7f4e0337109c2d4d03ea339812f934792387d
Nate Mann <nathan.mann@me.com>
2025-02-08 14:15:18 -0500

    added NumberFormatter extensions
    fix

--- FoundationExtensions/NumberFormatter Extensions.swift
+++ FoundationExtensions/NumberFormatter Extensions.swift
@@ -1,10 +1,25 @@
 import Foundation
 
 extension NumberFormatter {
-    
-    public convenience init(numberStyle: NumberFormatter.Style) {
-        self.init()
-        self.numberStyle = numberStyle
-    }
+
+  public convenience init(numberStyle: NumberFormatter.Style) {
+    self.init()
+    self.numberStyle = numberStyle
+  }
+
+  public func string(from double: Double) -> String {
+    string(from: NSNumber(value: double))!
+  }
+
+  public func string(from double: Double?) -> String? {
+    guard let value = double else { return nil }
+    return string(from: NSNumber(value: value))!
+  }
 }
 
+public let currencyFormatter = NumberFormatter()
+  |> set(\.numberStyle, .currency)
+  <> set(\.negativeFormat, "-¤#,##0.00")
+
+public let percentFormatter = NumberFormatter()
+  |> set(\.numberStyle, .percent)
